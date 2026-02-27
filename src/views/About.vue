fad52cc5112f8236ec5378228c07d4057d689862
Llego-250 <solideaze1251@gmail.com>
2026-02-27 22:12:17 +0200

    Creating About page

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>About VolunteerHub</h1>
+    <p>Connecting volunteers with organizations to make a positive impact in communities.</p>
+  </div>
+</template>
+
+<style scoped>
+.about { padding: 40px; max-width: 800px; margin: 0 auto; }
+</style>
