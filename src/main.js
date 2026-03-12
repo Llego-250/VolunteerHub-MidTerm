3b453d2b0950825f311489dbbf7170e1deae73b2
Llego-250 <solideaze1251@gmail.com>
2026-03-12 16:25:59 +0200

    theme toggle creation  style

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
+import './styles/theme.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App.vue'
 import router from './router'
+import { useThemeStore } from './stores/theme'
 
+const pinia = createPinia()
 const app = createApp(App)
-app.use(createPinia())
+
+app.use(pinia)
 app.use(router)
 app.mount('#app')
+
+// Initialize theme
+const themeStore = useThemeStore()
+themeStore.initTheme()
+
