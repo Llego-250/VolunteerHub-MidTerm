7ae450ca2cdf3a19cbf3417be9690307e3bb0eac
Llego-250 <solideaze1251@gmail.com>
2026-03-12 13:25:38 +0200

    Both navbars now show only the user avatar without the name. The avatar displays the user's profile picture if available, or their initial letter otherwise.

--- src/components/common/DashboardNavbar.vue
+++ src/components/common/DashboardNavbar.vue
@@ -5,23 +5,20 @@
         <router-link to="/" class="nav-logo">VolunteerHub</router-link>
         
         <div class="nav-actions">
-          <div class="user-info">
-            <span class="user-name">{{ authStore.currentUser?.name }}</span>
-            <div class="user-dropdown">
-              <div class="avatar-circle" @click="userDropdownOpen = !userDropdownOpen">
-                <img v-if="authStore.currentUser?.profilePic" :src="authStore.currentUser.profilePic" alt="Profile" />
-                <span v-else>{{ authStore.currentUser?.name?.charAt(0).toUpperCase() }}</span>
-              </div>
-              <div v-if="userDropdownOpen" class="dropdown-menu">
-                <button @click="handleLogout" class="dropdown-item">
-                  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
-                    <polyline points="16 17 21 12 16 7"/>
-                    <line x1="21" y1="12" x2="9" y2="12"/>
-                  </svg>
-                  Logout
-                </button>
-              </div>
+          <div class="user-dropdown">
+            <div class="avatar-circle" @click="userDropdownOpen = !userDropdownOpen">
+              <img v-if="authStore.currentUser?.profilePic" :src="authStore.currentUser.profilePic" alt="Profile" />
+              <span v-else>{{ authStore.currentUser?.name?.charAt(0).toUpperCase() }}</span>
+            </div>
+            <div v-if="userDropdownOpen" class="dropdown-menu">
+              <button @click="handleLogout" class="dropdown-item">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                  <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
+                  <polyline points="16 17 21 12 16 7"/>
+                  <line x1="21" y1="12" x2="9" y2="12"/>
+                </svg>
+                Logout
+              </button>
             </div>
           </div>
         </div>
